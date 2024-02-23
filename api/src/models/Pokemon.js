9fa95fb3586f60f2d998b2b78f35d30b06b4a0d5
kebrito <kellybritomejia@gmail.com>
2024-02-22 20:51:17 -0500

    Creando los modelos

--- api/src/models/Pokemon.js
+++ api/src/models/Pokemon.js
@@ -3,10 +3,65 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('pokemon', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  const Pokemon = sequelize.define(
+    "Pokemon",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+        unique: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        isUrl: true,
+        allowNull: false,
+      },
+      ps: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      atk: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      def: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      vel: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      height: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      weight: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
     },
+    { timestamps: false }
+  );
+
+  Pokemon.beforeCreate(async (pokemon) => {
+    try {
+      const mxId = await Pokemon.max("id");
+
+      //Se verifica si el pokemon que se está a punto de crear tiene un Id asignado si no, se asigna un nuevo Id. El nuevo Id se establece como el valor máximo actual más uno, o 152 si no hay registros en la tabla.
+
+      if (!pokemon.id) {
+        pokemon.id = mxId ? mxId + 1 : 152;
+      }
+    } catch (error) {
+      console.error("Excediste el numero maximo de ID:", error);
+      throw error;
+    }
   });
 };
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (<div>Estoy Trabajando Bien</div>)
 }
 
 export default App;
